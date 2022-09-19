17781242c33f5b72c16361bfb9f3ea3dc459e108
blacktea.hw <blacktea.hw@alibaba-inc.com>
2022-09-19 10:42:55 +0800

    create demo project

---
+++ BTObjectiveCDetail/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BTObjectiveCDetail
+//
+//  Created by blacktea on 19/9/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BTObjectiveCDetail/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BTObjectiveCDetail
+//
+//  Created by blacktea on 19/9/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
